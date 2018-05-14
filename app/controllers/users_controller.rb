ed2ad48491d278a9187c8e0180f2442561a266ac
ryotaro kumakawa <a1817rk@aiit.ac.jp>
2018-05-14 15:47:23 +0000

    Finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,39 @@
 class UsersController < ApplicationController
-  def new
+  #GET /users/:id
+  def show
+    @user = User.first
+    @user = User.find(params[:id])
+    # =>app/views/users/show.html.erb
+    # debugger
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    # =>これだけだとセキュリティ上の穴になってしまう（パラメタの中身を精査していないため）
+    #@user.name  = params[:user][name]
+    #@user.email = params[:user][email]
+    #params[:user][password]のこと
+    if @user.save # => Validation
+      #Sucess
+      #redirect_to users_path(@user.id)
+      #redirect_to users_path(@user) 
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # =>GET "/users/#{@user.id}"  =>show
+    else
+      # Failure
+      render 'new'
+    end
+  end
+  
+  def user_params
+    params.require(:user).permit(
+      :name, :email, :password, 
+      :password_confirmation)
+  end
+  
 end
